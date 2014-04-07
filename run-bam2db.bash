710c6bebcc8ca41c1e75a9bce62477171b598f51
George Kettleborough (TGAC) <george.kettleborough@tgac.ac.uk>
2014-04-07 10:39:43 +0100

    Don't use aln in bam filenames

--- run-bam2db.bash
+++ run-bam2db.bash
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-# the bam files must be named like <animal>-d<day>-aln.bam
+# the bam files must be named like <animal>-d<day>.bam
 
-prog=$1                       # the bam2db program
+prog=$1                         # the bam2db program
 db=$2                           # the database file
 dir=$3                          # the diectory with bams in it
 
 samtoolscmd="samtools mpileup -BQ0 -d10000000 "
 
-main=$(ls -1 ${dir}/*.bam | xargs -n1 basename | sed -nr '/^F[0-9]+_d[0-9]+_aln\.bam$/p')
-misc=$(ls -1 ${dir}/*.bam | xargs -n1 basename | sed -r  '/^F[0-9]+_d[0-9]+_aln\.bam$/d')
+main=$(ls -1 ${dir}/*.bam | xargs -n1 basename | sed -nr '/^F[0-9]+_d[0-9]+\.bam$/p')
+misc=$(ls -1 ${dir}/*.bam | xargs -n1 basename | sed -r  '/^F[0-9]+_d[0-9]+\.bam$/d')
 
-animaldays=$(echo "$main" | sed -r 's/^F([0-9]+)_d([0-9]+)_aln\.bam$/\1:\2/g' | uniq)
-miscnames=$(echo "$misc" | sed -r 's/^(.*)_aln\.bam$/\1/g' | uniq)
+animaldays=$(echo "$main" | sed -r 's/^F([0-9]+)_d([0-9]+)\.bam$/\1:\2/g' | uniq)
+miscnames=$(echo "$misc" | sed -r 's/^(.*)\.bam$/\1/g' | uniq)
 
 for animalday in $animaldays; do
     a=${animalday%:*}
     d=${animalday#*:}
     echo "F${a}_d${d}"
-    $samtoolscmd ${dir}/F${a}_d${d}_aln.bam | ${prog} ${db} F$a $d
+    $samtoolscmd ${dir}/F${a}_d${d}.bam | ${prog} ${db} F$a $d
 done
 
 for name in $miscnames; do
     echo "${name}"
-    $samtoolscmd ${dir}/${name}_aln.bam | ${prog} ${db} $name 0
+    $samtoolscmd ${dir}/${name}.bam | ${prog} ${db} $name 0
 done
 
