4e0cca8047745b6d0800ec11c99db98ae9a921bf
George Kettleborough (TGAC) <george.kettleborough@tgac.ac.uk>
2014-05-08 11:52:30 +0100

    Fix read-lengths.py

--- read-lengths.py
+++ read-lengths.py
@@ -17,18 +17,20 @@ for line in file:
             exit("Malformed file, line {:d}".format(lineno))
         state = 1
     elif state == 1:
-        lengths.append(len(line))
+        lengths.append(len(line) - 1)
         state = 2
     elif state == 2:
         if line[0] != '+':
             exit("Malformed file, line {:d}".format(lineno))
         state = 3
     elif state == 3:
-        if len(line) != lengths[-1]:
+        if (len(line) - 1) != lengths[-1]:
             exit("Malformed file, line {:d}".format(lineno))
         state = 0
     lineno += 1
 
-print "Min: {:.1f}, max: {:.1f}, mean: {:.1f}".format(
-    min(lengths), max(lengths), sum(lengths)/len(lengths))
+# print "Min: {:.1f}, max: {:.1f}, mean: {:.1f}".format(
+#     min(lengths), max(lengths), sum(lengths)/len(lengths))
 
+for length in lengths:
+    print length;
