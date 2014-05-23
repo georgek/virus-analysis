249a28f44472e73d305245354c3e510a6e1ddedb
George Kettleborough (TGAC) <george.kettleborough@tgac.ac.uk>
2014-05-23 14:31:03 +0100

    Add bam-dr

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 CFLAGS ?= -Wall -O2 -g -I$(HOME)/include -L$(HOME)/lib
 
-all: bam2db trim myccs
+all: bam2db trim myccs bam-dr
 
 bam2db: bam2db.c
-	gcc $(CFLAGS) bam2db.c -o bam2db -lsqlite3
+	gcc $(CFLAGS) bam2db.c -o bam2db -lsqlite3 -lbam -lz
 
 trim: trim.c
 	gcc $(CFLAGS) trim.c -o trim
@@ -11,5 +11,11 @@ trim: trim.c
 myccs: myccs.c
 	gcc $(CFLAGS) -pthread myccs.c -o myccs -lbam -lz
 
+bam-dr:	bam-dr.c
+	gcc $(CFLAGS) -pthread bam-dr.c -o bam-dr -lbam -lz
+
+bamtest: bamtest.c
+	gcc $(CFLAGS) -pthread bamtest.c -o bamtest -lbam -lz
+
 clean:
-	rm -f bam2db trim myccs
+	rm -f bam2db trim myccs bam-dr
---
+++ bam-dr.c
@@ -0,0 +1,49 @@
+/* Counts the number of duplicates removed by looking at the DR field in each
+ * read (if it exists). The DR field is updated with patched version of
+ * bam_rmdup.c (samtools) */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <bam/sam.h>
+
+int main(int argc, char *argv[])
+{
+     char *filename = NULL;
+     samfile_t *in;
+     bam1_t *b;
+     uint8_t *dr = 0;
+     uint32_t dr_count = 0;
+
+     b = bam_init1();
+
+     if (argc < 2) {
+          fprintf(stderr, "Usage: %s <bamfile>\n", argv[0]);
+          exit(1);
+     }
+     else {
+          filename = argv[1];
+     }
+     printf("filename: %s\n", filename);
+
+     in = samopen(filename, "rb", NULL);
+     if (!in) {
+          fprintf(stderr, "Error opening file %s\n", filename);
+     }
+
+     while (samread(in, b) >= 0) {
+          
+          dr = bam_aux_get(b, "DR");
+          if (dr) {
+               dr_count += bam_aux2i(dr);
+          }
+     }
+
+     samclose(in);
+     bam_destroy1(b);
+
+     printf("Duplicates removed: %u\n", dr_count);
+
+     return 0;
+}
