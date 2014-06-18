9de95e212a202d6f89827dade16fe651ea4e86ac
George Kettleborough (TGAC) <george.kettleborough@tgac.ac.uk>
2014-06-18 12:02:14 +0100

    Add SQL insertion

--- bam2db2.c
+++ bam2db2.c
@@ -2,83 +2,197 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include <string.h>
+#include <limits.h>
 
 #include <sqlite3.h>
 
 #include <bam/sam.h>
 #include <bam/bam.h>
 
-static sqlite3 *db = NULL;
-static sqlite3_stmt *stmt;
-static samfile_t *bamin;
-static bam1_t *bam_read;
-static char *dbname, *bamfilename, *animal;
-static int day;
+#include "errors.h"
 
-char sql_select_chrid[] = "select rowid from chromosomes where name = ?;";
+static const size_t win_len = 500;
 
-int animal_id(void* ret_val, int ncols, char **vals, char **names)
+typedef struct pos_nucs {
+     sqlite3_int64 forward[4];
+     sqlite3_int64 reverse[4];
+     sqlite3_int64 dels;
+} PosNucs;
+
+char sql_select_chrid[] = "SELECT id FROM chromosomes WHERE name = ?;";
+char sql_select_animalid[] = "SELECT id FROM animals WHERE name = ?;";
+char sql_nuc_insert[] = "INSERT INTO nucleotides VALUES(?1, ?2, ?3, "
+     "?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13)";
+
+typedef struct win_data {
+     int32_t beg;
+     PosNucs *posarr;
+} WinData;
+
+/* print sqlite error */
+static int nerrors = 0, maxerrors = 10;
+void do_error(char **errormessage)
 {
-     if (1 == ncols){
-          *(int *)ret_val = strtoul(vals[0], NULL, 10);
-          return 0;
-     }
-     else {
-          return 1;
+     if (*errormessage) {
+          fprintf(stderr, "SQLite3 error: %s\n", *errormessage);
+          sqlite3_free(*errormessage);
+          *errormessage = NULL;
+          nerrors++;
+          if (nerrors >= maxerrors) {
+               fprintf(stderr, "(Stopping after %d errors.)\n", maxerrors);
+               exit(SQLITE_ERROR);
+          }
      }
 }
 
+/* callback for bam_fetch() */
+static int fetch_func(const bam1_t *b, void *data)
+{
+     bam_plbuf_t *buf = data;
+     if (b->core.flag & BAM_FPROPER_PAIR) {
+          bam_plbuf_push(b, buf);
+     }
+     return 0;
+}
+
+/* callback for bam_plbuf_init() */
+static int pileup_func(uint32_t tid, uint32_t pos, int n,
+                       const bam_pileup1_t *pl, void *data)
+{
+     WinData *win = data;
+     int win_pos, i;
+     sqlite_int64 *nucs;
+
+     win_pos = pos - win->beg;
+     if (win_pos >= 0 && win_pos < win_len) {
+          for (i = 0; i < n; ++i) {
+               if (pl[i].is_del) {
+                    win->posarr[win_pos].dels++;
+               }
+               else {
+                    if (pl[i].b->core.flag & BAM_FREVERSE) {
+                         nucs = win->posarr[win_pos].reverse;
+                    }
+                    else {
+                         nucs = win->posarr[win_pos].forward;
+                    }
+                    switch(bam1_seqi(bam1_seq(pl[i].b), pl[i].qpos)) {
+                    case 1:
+                         nucs[0]++;
+                         break;
+                    case 2:
+                         nucs[1]++;
+                         break;
+                    case 4:
+                         nucs[2]++;
+                         break;
+                    case 8:
+                         nucs[3]++;
+                         break;
+                    }
+               }
+          }
+     }
+
+     return 0;
+}
+
+static int window_to_db(sqlite3 *db, sqlite3_stmt *stmt,
+                        sqlite3_int64 chr_id, uint32_t chr_len, WinData *win) 
+{
+     int i;
+     int res_code;
+     size_t win_len_used = chr_len - win->beg;
+     win_len_used = win_len_used < win_len ? win_len_used : win_len;
+
+     for (i = 0; i < win_len_used; ++i) {
+          sqlite3_bind_int64(stmt, 3, chr_id);
+          sqlite3_bind_int64(stmt, 4, win->beg + i + 1);
+          sqlite3_bind_int64(stmt, 5, win->posarr[i].forward[0]);
+          sqlite3_bind_int64(stmt, 6, win->posarr[i].forward[1]);
+          sqlite3_bind_int64(stmt, 7, win->posarr[i].forward[2]);
+          sqlite3_bind_int64(stmt, 8, win->posarr[i].forward[3]);
+          sqlite3_bind_int64(stmt, 9, win->posarr[i].reverse[0]);
+          sqlite3_bind_int64(stmt, 10, win->posarr[i].reverse[1]);
+          sqlite3_bind_int64(stmt, 11, win->posarr[i].reverse[2]);
+          sqlite3_bind_int64(stmt, 12, win->posarr[i].reverse[3]);
+          sqlite3_bind_int64(stmt, 13, win->posarr[i].dels);
+
+          res_code = sqlite3_step(stmt);
+          if (res_code != SQLITE_DONE) {
+               fprintf(stderr, "SQLite3 error: %d!\n", res_code);
+               exit(SQL_ERROR);
+          }
+
+          sqlite3_reset(stmt);
+     }
+     return 0;
+}
+     
 int main(int argc, char *argv[])
 {
+     /* char *progname; */
+     /* int verbose; */
+
+     char *dbname;
+     char *bamfilename;
+     char *animal;
+     int day;
+
+     sqlite3 *db = NULL;
+     sqlite3_stmt *stmt;
+     samfile_t *bamin;
+     bam_index_t *bamidx;
+
      int dbopencode = 0;
      char *errormessage = NULL;
-     /* uint8_t *dr = NULL; */
-     /* uint32_t dr_count = 0; */
-     int i;
      int n_chr;
-     sqlite3_int64 *chr_rowids = NULL;
+     sqlite3_int64 *chr_ids = NULL;
+     sqlite3_int64 animal_id;
      int res_code;
 
-     int32_t tid;
-
-     unsigned *chr_mapped = NULL;
-     unsigned *chr_unmapped = NULL;
-
-     bam_read = bam_init1();
+     bam_plbuf_t *buf;
+     size_t i, j;
+     WinData win;
 
      if (argc < 5) {
-          fprintf(stderr, "Usage: %s database bamfile animal day\n", argv[0]);
-          exit(1);
+          printf("Usage: %s database bamfile animal day\n", argv[0]);
+          exit(ARG_ERROR);
      }
      else {
-          printf("DB name: %s\n", argv[1]);
+          fprintf(stderr, "DB name: %s... ", argv[1]);
           dbname = argv[1];
           bamfilename = argv[2];
           animal = argv[3];
-          day = strtoul(argv[3], NULL, 10);
+          day = strtoul(argv[4], NULL, 10);
      }
      /* try to open db */
-     dbopencode = sqlite3_open(argv[1], &db);
+     dbopencode = sqlite3_open(dbname, &db);
      if (SQLITE_OK == dbopencode) {
-          printf("OK\n");
+          fprintf(stderr,"OK\n");
      }
      else {
           printf("SQLite3 error: %d\n", dbopencode);
-          exit(1);
+          exit(SQL_ERROR);
      }
 
      /* try to open bam file */
      bamin = samopen(bamfilename, "rb", NULL);
      if (!bamin) {
           fprintf(stderr, "Error opening bamfile %s\n", bamfilename);
-          exit(1);
+          exit(BAM_ERROR);
+     }
+     /* try to open index */
+     bamidx = bam_index_load(bamfilename);
+     if (!bamidx) {
+          fprintf(stderr, "Error opening index for %s\n", bamfilename);
+          exit(BAM_ERROR);
      }
 
-     /* get rowids of chromosomes */
+     /* get ids of chromosomes */
      n_chr = bamin->header->n_targets;
-     chr_rowids = malloc(sizeof(sqlite3_int64) * n_chr);
-     printf("targets: %d\n", n_chr);
-     sqlite3_prepare_v2(db, sql_select_chrid, strlen(sql_select_chrid)+1,
+     chr_ids = malloc(sizeof(sqlite3_int64) * n_chr);
+     sqlite3_prepare_v2(db, sql_select_chrid, sizeof(sql_select_chrid),
                         &stmt, NULL);
      for (i = 0; i < n_chr; ++i) {
           sqlite3_bind_text(stmt, 1, bamin->header->target_name[i], -1,
@@ -86,57 +200,69 @@ int main(int argc, char *argv[])
           res_code = sqlite3_step(stmt);
           if (res_code != SQLITE_ROW) {
                fprintf(stderr, "SQLite3 error: %d!\n", res_code);
-               exit(1);
+               exit(SQL_ERROR);
           }
-          chr_rowids[i] = sqlite3_column_int64(stmt, 0);
+          chr_ids[i] = sqlite3_column_int64(stmt, 0);
           sqlite3_reset(stmt);
-          printf("%s, %d, %ld\n", bamin->header->target_name[i],
-                 i, (long)chr_rowids[i]);
      }
      sqlite3_finalize(stmt);
+     /* get id of animal */
+     sqlite3_prepare_v2(db, sql_select_animalid, sizeof(sql_select_animalid),
+                        &stmt, NULL);
+     sqlite3_bind_text(stmt, 1, animal, -1, SQLITE_STATIC);
+     res_code = sqlite3_step(stmt);
+     if (res_code != SQLITE_ROW) {
+          fprintf(stderr, "SQLite3 error: %d!\n", res_code);
+          exit(SQL_ERROR);
+     }
+     animal_id = sqlite3_column_int64(stmt, 0);
+     sqlite3_finalize(stmt);
      
-     /* increases speed of insertion (means if the program crashes the db is
-      * left invalid) */
+     /* increases speed of insertion (means if the program crashes the db is */
+     /* left invalid) */
      sqlite3_exec(db, "PRAGMA synchronous=OFF", NULL, NULL, &errormessage);
      sqlite3_exec(db, "PRAGMA temp_store=MEMORY", NULL, NULL, &errormessage);
      sqlite3_exec(db, "PRAGMA journal_mode=MEMORY", NULL, NULL, &errormessage);
 
-     /* while (samread(bamin, bam_read) >= 0) { */
-     /*      dr = bam_aux_get(bam_read, "DR"); */
-     /*      if (dr) { */
-     /*           dr_count += bam_aux2i(dr); */
-     /*      } */
-     /* } */
+     win.posarr = calloc(win_len, sizeof(PosNucs));
 
-     chr_mapped = calloc(n_chr + 1, sizeof(unsigned));
-     chr_unmapped = calloc(n_chr + 1, sizeof(unsigned));
-     while (samread(bamin, bam_read) >= 0) {
-          tid = bam_read->core.tid;
-          if (bam_read->core.flag & BAM_FUNMAP) {
-               /* unmapped */
-               chr_unmapped[tid < 0 ? n_chr : tid]++;
-          }
-          else {
-               chr_mapped[tid < 0 ? n_chr : tid]++;
-          }
-     }
+     buf = bam_plbuf_init(&pileup_func, &win);
+     /* disable maximum pileup depth */
+     bam_plp_set_maxcnt(buf->iter, INT_MAX);
+
+     /* start prepared statement for nucleotide insert */
+     sqlite3_prepare_v2(db, sql_nuc_insert, sizeof(sql_nuc_insert),
+                        &stmt, NULL);
+     sqlite3_bind_int64(stmt, 1, animal_id);
+     sqlite3_bind_int(stmt, 2, day);
+     
+     sqlite3_exec(db, "BEGIN TRANSACTION;", NULL, NULL, &errormessage);
+     do_error(&errormessage);
      for (i = 0; i < n_chr; ++i) {
-          printf("%-4s %5"PRIu32" %8u %8u\n",
-                 bamin->header->target_name[i], bamin->header->target_len[i],
-                 chr_mapped[i], chr_unmapped[i]);
+          fprintf(stderr, "%s", bamin->header->target_name[i]);
+          for (j = 0; j < bamin->header->target_len[i]; j += win_len) {
+               win.beg = j;
+               bam_fetch(bamin->x.bam, bamidx,
+                         i, j, j + win_len,
+                         buf, &fetch_func);
+               bam_plbuf_push(0, buf);
+               window_to_db(db, stmt,
+                            chr_ids[i], bamin->header->target_len[i], &win);
+               bam_plbuf_reset(buf);
+               memset(win.posarr, 0, sizeof(PosNucs)*win_len);
+               fprintf(stderr, ".");
+          }
+          fprintf(stderr, "\n");
      }
-     printf("%-4s %5d %8u %8u\n", "*", 0, 
-            chr_mapped[n_chr], chr_unmapped[n_chr]);
+     sqlite3_exec(db, "COMMIT TRANSACTION;", NULL, NULL, &errormessage);
+     do_error(&errormessage);
+     sqlite3_finalize(stmt);
 
+     free(win.posarr);
+     bam_index_destroy(bamidx);
      samclose(bamin);
-     bam_destroy1(bam_read);
-     free(chr_rowids);
-     free(chr_mapped);
-     free(chr_unmapped);
-
+     free(chr_ids);
      sqlite3_close(db);
-
-     /* printf("Duplicates removed: %u\n", dr_count); */
 
      return 0;
 }
