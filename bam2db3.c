b3a96e281662458366f68a4674d51978989827af
George Kettleborough (TGAC) <george.kettleborough@tgac.ac.uk>
2014-07-17 16:53:04 +0100

    Different error for no rows returned

--- bam2db3.c
+++ bam2db3.c
@@ -185,6 +185,10 @@ void sqlite3_step_onerow(sqlite3_stmt *stmt)
 {
      int res_code;
      res_code = sqlite3_step(stmt);
+     if (res_code == SQLITE_DONE) {
+          fprintf(stderr, "No rows returned for query: %s\n",
+                  sqlite3_sql(stmt));
+     }
      if (res_code != SQLITE_ROW) {
           fprintf(stderr, "SQLite3 error: %d (%s)\n",
                   res_code, sqlite3_sql(stmt));
