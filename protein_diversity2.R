466fbbe85c31915a1bbe2727ddcb8e1e7098a58d
George Kettleborough (TGAC) <george.kettleborough@tgac.ac.uk>
2014-08-14 13:58:19 +0100

    Make protein div work for all samples

--- protein_diversity2.R
+++ protein_diversity2.R
@@ -1,3 +1,6 @@
+## this is pretty inefficient because it rebuilds the protein, consensus and
+## diversity tables for each animal/day
+
 rbinde <- function(f1,f2) {
     if (length(f1)==0) {
         return(f2)
@@ -12,8 +15,6 @@ args
 databaseFile <- args[1]
 outputFileFormat <- args[2]
 titleAddition <- args[3]
-animalId <- args[4]
-day <- args[5]
 
 library("DBI")
 library("RSQLite")
@@ -24,29 +25,38 @@ db <- dbConnect(dbDriver("SQLite"), dbname=databaseFile)
 
 genes <- dbGetQuery(db, "select id, name, product from genes;")
 
-dat.cov <- list()
-dat.var <- list()
-lengths <- list()
+animals <- dbGetQuery(db, "select id, name from animals;")
 
-for (i in 1:length(genes$id)) {
-    print(sprintf("Gene %s (%s)", genes$name[i], genes$product[i]))
-    dat.cov[[i]] <- list()
-    dat.var[[i]] <- list()
-    lengths[[i]] <- 0
-    next_start <- 0
-    cds_regions <- dbGetQuery(db, sprintf("select chromosome, start, end, strand from cds_regions
-join cds on (cds.id = cds) where gene = %s;", genes$id[i]))
-    for (j in 1:length(cds_regions$start)) {
-        print(sprintf("cds: %s: %s - %s (%s)", cds_regions$chromosome[j], cds_regions$start[j],
-                      cds_regions$end[j], cds_regions$strand[j]))
-        dbGetQuery(db, (sprintf("create temp view protein as select animal, day, (position-%s)/3+1 as pos,
+for (i in 1:length(animals$id)) {
+    days <- dbGetQuery(db, sprintf("select distinct day from nucleotides where animal = %d;", animals$id[i]))$day
+    if (length(days) == 0) {
+        next
+    }
+
+    for (j in 1:length(days)) {
+        dat.cov <- list()
+        dat.var <- list()
+        lengths <- list()
+
+        for (k in 1:length(genes$id)) {
+            print(sprintf("Gene %s (%s)", genes$name[k], genes$product[k]))
+            dat.cov[[k]] <- list()
+            dat.var[[k]] <- list()
+            lengths[[k]] <- 0
+            next_start <- 0
+            cds_regions <- dbGetQuery(db, sprintf("select chromosome, start, end, strand from cds_regions
+join cds on (cds.id = cds) where gene = %s;", genes$id[k]))
+            for (l in 1:length(cds_regions$start)) {
+                print(sprintf("cds: %s: %s - %s (%s)", cds_regions$chromosome[l], cds_regions$start[l],
+                              cds_regions$end[l], cds_regions$strand[l]))
+                dbGetQuery(db, (sprintf("create temp view protein as select animal, day, (position-%s)/3+1 as pos,
 Ala,Arg,Asn,Asp,Cys,Gln,Glu,Gly,His,Ile,Leu,Lys,Met,Phe,Pro,Ser,Thr,Trp,Tyr,Val,STOP
 from amino_acids where chromosome = %s and position >= %s and position <= %s and (position-%s)%%3 = 0;",
-                               cds_regions$start[j], cds_regions$chromosome[j],
-                               cds_regions$start[j], cds_regions$end[j],
-                               cds_regions$start[j])))
-        ## make consensus protein
-        dbGetQuery(db, "create temp view consensus as
+                                        cds_regions$start[l], cds_regions$chromosome[l],
+                                        cds_regions$start[l], cds_regions$end[l],
+                                        cds_regions$start[l])))
+                ## make consensus protein
+                dbGetQuery(db, "create temp view consensus as
 select pos,case max(sum(Ala),sum(Arg),sum(Asn),sum(Asp),sum(Cys),
                     sum(Gln),sum(Glu),sum(Gly),sum(His),sum(Ile),
                     sum(Leu),sum(Lys),sum(Met),sum(Phe),sum(Pro),
@@ -77,7 +87,7 @@ select pos,case max(sum(Ala),sum(Arg),sum(Asn),sum(Asp),sum(Cys),
 from protein
 group by pos;")
 
-        dbGetQuery(db, "create temp view div as
+                dbGetQuery(db, "create temp view div as
 select animal,day,pos,
 case acid when 'Ala' then Ala else -Ala end Ala,
 case acid when 'Arg' then Arg else -Arg end Arg,
@@ -103,36 +113,38 @@ case acid when 'STOP' then STOP else -STOP end STOP
 from protein
 join consensus using (pos);")
 
-        prot <- dbGetQuery(db, "select pos,Ala,Arg,Asn,Asp,Cys,Gln,Glu,Gly,His,Ile,Leu,Lys,Met,Phe,Pro,Ser,Thr,Trp,Tyr,Val,STOP
-from div;")
-        prot$pos <- prot$pos + next_start
-        next_start <- max(prot$pos)+1
-        prot <- melt(prot, id.vars=c("pos"), variable.name="acid", value.name="count")
-        dat.cov[[i]] <- rbinde(dat.cov[[i]], subset(prot, count > 0))
-        dat.var[[i]] <- rbinde(dat.var[[i]], subset(prot, count < 0))
-        lengths[[i]] <- lengths[[i]] + (max(prot$pos) - min(prot$pos))
+                prot <- dbGetQuery(db, sprintf("select pos,Ala,Arg,Asn,Asp,Cys,Gln,Glu,Gly,His,Ile,Leu,Lys,Met,Phe,Pro,Ser,Thr,Trp,Tyr,Val,STOP
+from div where animal = %d and day = %d;", animals$id[i], days[j]))
+                prot$pos <- prot$pos + next_start
+                next_start <- max(prot$pos)+1
+                prot <- melt(prot, id.vars=c("pos"), variable.name="acid", value.name="count")
+                dat.cov[[k]] <- rbinde(dat.cov[[k]], subset(prot, count > 0))
+                dat.var[[k]] <- rbinde(dat.var[[k]], subset(prot, count < 0))
+                lengths[[k]] <- lengths[[k]] + (max(prot$pos) - min(prot$pos))
 
-        dbGetQuery(db, "drop view div;")
-        dbGetQuery(db, "drop view consensus;")
-        dbGetQuery(db, "drop view protein;")
+                dbGetQuery(db, "drop view div;")
+                dbGetQuery(db, "drop view consensus;")
+                dbGetQuery(db, "drop view protein;")
+            }
+        }
+
+        ## set up PDF
+        maxlength <- max(unlist(lengths))
+        filename <- sprintf(outputFileFormat, animals$name[i], days[j])
+        print(filename)
+        pdf(file=filename, height=8, width=(maxlength / 10), onefile=TRUE)
+
+        ## print plots
+        for (k in 1:length(genes$id)) {
+            c <- ggplot()
+            c <- c + geom_bar(data=dat.cov[[k]], aes(x=pos, y=count, fill=acid), stat="identity")
+            c <- c + geom_bar(data=dat.var[[k]], aes(x=pos, y=count, fill=acid), stat="identity")
+            c <- c + scale_x_discrete(expand=c(0, (maxlength-lengths[[k]])/2 + 1))
+            c <- c + labs(title = sprintf("Protein diversity in %s day %s, %s", animals$name[i], days[j], genes$product[k]),
+                          x = "Position", y = "Coverage", fill = "Acid")
+            c <- c + theme(axis.text.x = element_text(size=8,angle=90, hjust=1))
+            suppressWarnings(print(c))  # ggplot warns about negative bars, but it's ok
+        }
+        dev.off()
     }
 }
-
-## set up PDF
-maxlength <- max(unlist(lengths))
-filename <- sprintf(outputFileFormat, animalId, day)
-print(filename)
-pdf(file=filename, height=8, width=(maxlength / 10), onefile=TRUE)
-
-## print plots
-for (i in 1:length(genes$id)) {
-    c <- ggplot()
-    c <- c + geom_bar(data=dat.cov[[i]], aes(x=pos, y=count, fill=acid), stat="identity")
-    c <- c + geom_bar(data=dat.var[[i]], aes(x=pos, y=count, fill=acid), stat="identity")
-    c <- c + scale_x_discrete(expand=c(0, (maxlength-lengths[[i]])/2 + 1))
-    c <- c + labs(title = sprintf("Protein diversity in %s day %s, %s", animalId, day, genes$product[i]),
-                  x = "Position", y = "Coverage", fill = "Acid")
-    c <- c + theme(axis.text.x = element_text(size=8,angle=90, hjust=1))
-    suppressWarnings(print(c))  # ggplot warns about negative bars, but it's ok
-}
-dev.off()
